09695cb5bdf6da008c0439c3de75d01213e7ecae
Subhash154 <subhashji9861@gmail.com>
2025-11-12 16:12:25 +0530

    Update email address in config.py

--- 2nd-project/flask-todo-app-py-project/config.py
+++ 2nd-project/flask-todo-app-py-project/config.py
@@ -5,4 +5,4 @@ class Config:
     AWS_REGION = 'us-east-1'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
-    EMAIL = "sbiradar2185@gmail.com"
+    EMAIL = "subhashji9861@gmail.com"
